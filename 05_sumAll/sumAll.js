e3041e03cfbf64f21f5d2b8664585bb3a6e52c72
Himanth24 <himanthgodari@gmail.com>
2023-06-26 14:46:18 +0530

    Solved until tempConversion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(a<0 || b<0) return 'ERROR';
+    if(typeof b != "number" || typeof a != "number") return 'ERROR';
+    let sum = 0,temp;
+    if(a>b) {
+        temp = a; a = b; b = temp;
+    }
+    for(let i=a;i<=b;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
